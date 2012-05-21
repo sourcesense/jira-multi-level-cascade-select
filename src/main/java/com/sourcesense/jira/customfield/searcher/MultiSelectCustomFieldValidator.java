beeaddf397de2e2e16fa83f911b420f5927e225d
alessandro.benedetti <alessandro.benedetti@sourcesense.com>
2012-05-21 15:06:10 +0200

    test commit

--- src/main/java/com/sourcesense/jira/customfield/searcher/MultiSelectCustomFieldValidator.java
+++ src/main/java/com/sourcesense/jira/customfield/searcher/MultiSelectCustomFieldValidator.java
@@ -1,6 +1,5 @@
 package com.sourcesense.jira.customfield.searcher;
 
-
 import static com.atlassian.jira.util.dbc.Assertions.notNull;
 
 import java.util.List;
@@ -20,90 +19,78 @@ import com.atlassian.query.operand.Operand;
 import com.opensymphony.user.User;
 
 /**
- * A validator for select custom fields. Takes into account
- * if the user has any context under which she can see the given options.
- *
+ * A validator for select custom fields. Takes into account if the user has any context under which
+ * she can see the given options.
+ * 
  * @since v4.0
  */
-public class MultiSelectCustomFieldValidator implements ClauseValidator
-{
-    private final SupportedOperatorsValidator supportedOperatorsValidator;
-    private final CustomField customField;
-    private final JqlSelectOptionsUtil jqlSelectOptionsUtil;
-    private final JqlOperandResolver jqlOperandResolver;
-    private final I18nHelper.BeanFactory beanFactory;
+public class MultiSelectCustomFieldValidator implements ClauseValidator {
+  private final SupportedOperatorsValidator supportedOperatorsValidator;
 
-    public MultiSelectCustomFieldValidator(final CustomField customField, final JqlSelectOptionsUtil jqlSelectOptionsUtil, final JqlOperandResolver jqlOperandResolver, final I18nHelper.BeanFactory beanFactory)
-    {
-        this.beanFactory = notNull("beanFactory", beanFactory);
-        this.customField = notNull("customField", customField);
-        this.jqlSelectOptionsUtil = notNull("jqlSelectOptionsUtil", jqlSelectOptionsUtil);
-        this.jqlOperandResolver = notNull("jqlOperandResolver", jqlOperandResolver);
-        this.supportedOperatorsValidator = getSupportedOperatorsValidator();
+  private final CustomField customField;
+
+  private final JqlSelectOptionsUtil jqlSelectOptionsUtil;
+
+  private final JqlOperandResolver jqlOperandResolver;
+
+  private final I18nHelper.BeanFactory beanFactory;
+
+  public MultiSelectCustomFieldValidator(final CustomField customField, final JqlSelectOptionsUtil jqlSelectOptionsUtil, final JqlOperandResolver jqlOperandResolver,
+          final I18nHelper.BeanFactory beanFactory) {
+    this.beanFactory = notNull("beanFactory", beanFactory);
+    this.customField = notNull("customField", customField);
+    this.jqlSelectOptionsUtil = notNull("jqlSelectOptionsUtil", jqlSelectOptionsUtil);
+    this.jqlOperandResolver = notNull("jqlOperandResolver", jqlOperandResolver);
+    this.supportedOperatorsValidator = getSupportedOperatorsValidator();
+  }
+
+  public MessageSet validate(final User searcher, final TerminalClause terminalClause) {
+    MessageSet messageSet = supportedOperatorsValidator.validate(searcher, terminalClause);
+    if (!messageSet.hasAnyMessages()) {
+      return validateValues(searcher, terminalClause);
+    } else {
+      return messageSet;
+    }
+  }
+
+  private MessageSet validateValues(final User searcher, final TerminalClause terminalClause) {
+    I18nHelper i18n = getI18n(searcher);
+    final MessageSet messageSet = new MessageSetImpl();
+
+    final Operand operand = terminalClause.getOperand();
+    final List<QueryLiteral> literals = jqlOperandResolver.getValues(searcher, operand, terminalClause);
+    if (literals == null || literals.isEmpty()) {
+      return messageSet;
     }
 
-    public MessageSet validate(final User searcher, final TerminalClause terminalClause)
-    {
-        MessageSet messageSet = supportedOperatorsValidator.validate(searcher, terminalClause);
-        if (!messageSet.hasAnyMessages())
-        {
-            return validateValues(searcher, terminalClause);
+    for (QueryLiteral literal : literals) {
+      if (!literal.isEmpty() && !literal.asString().contains(":")) {
+        final List<Option> options = getOptionsFromLiteral(searcher, literal);
+        if (options.isEmpty()) {
+          if (jqlOperandResolver.isFunctionOperand(literal.getSourceOperand())) {
+            messageSet.addErrorMessage(i18n.getText("jira.jql.clause.select.option.does.not.exist.function", literal.getSourceOperand().getName(), terminalClause.getName()));
+          } else {
+            messageSet.addErrorMessage(i18n.getText("jira.jql.clause.select.option.does.not.exist", literal.asString(), terminalClause.getName()));
+          }
         }
-        else
-        {
-            return messageSet;
-        }
+      }
     }
+    return messageSet;
+  }
 
-    private MessageSet validateValues(final User searcher, final TerminalClause terminalClause)
-    {
-        I18nHelper i18n = getI18n(searcher);
-        final MessageSet messageSet = new MessageSetImpl();
+  protected List<Option> getOptionsFromLiteral(final User searcher, final QueryLiteral literal) {
+    return jqlSelectOptionsUtil.getOptions(customField, (com.atlassian.crowd.embedded.api.User) searcher, literal, false);
+  }
 
-        final Operand operand = terminalClause.getOperand();
-        final List<QueryLiteral> literals = jqlOperandResolver.getValues(searcher, operand, terminalClause);
-        if (literals == null || literals.isEmpty())
-        {
-            return messageSet;
-        }
+  I18nHelper getI18n(User user) {
+    return beanFactory.getInstance(user);
+  }
 
-        for (QueryLiteral literal : literals)
-        {
-            if (!literal.isEmpty()&&!literal.asString().contains(":"))
-            {
-                final List<Option> options = getOptionsFromLiteral(searcher, literal);
-                if (options.isEmpty())
-                {
-                    if (jqlOperandResolver.isFunctionOperand(literal.getSourceOperand()))
-                    {
-                        messageSet.addErrorMessage(i18n.getText("jira.jql.clause.select.option.does.not.exist.function", literal.getSourceOperand().getName(), terminalClause.getName()));
-                    }
-                    else
-                    {
-                        messageSet.addErrorMessage(i18n.getText("jira.jql.clause.select.option.does.not.exist", literal.asString(), terminalClause.getName()));
-                    }
-                }
-            }
-        }
-        return messageSet;
-    }
+  // /CLOVER:OFF
 
-    protected List<Option> getOptionsFromLiteral(final User searcher, final QueryLiteral literal) 
-    {
-        return jqlSelectOptionsUtil.getOptions(customField, (com.atlassian.crowd.embedded.api.User) searcher, literal, false);
-    }
+  SupportedOperatorsValidator getSupportedOperatorsValidator() {
+    return new SupportedOperatorsValidator(OperatorClasses.EQUALITY_OPERATORS_WITH_EMPTY);
+  }
 
-    I18nHelper getI18n(User user)
-    {
-        return beanFactory.getInstance(user);
-    }
-
-    ///CLOVER:OFF
-
-    SupportedOperatorsValidator getSupportedOperatorsValidator()
-    {
-        return new SupportedOperatorsValidator(OperatorClasses.EQUALITY_OPERATORS_WITH_EMPTY);
-    }
-
-    ///CLOVER:ON
+  // /CLOVER:ON
 }
--- src/main/webapp/includes/js/multilevelcascadingUtil.js
+++ src/main/webapp/includes/js/multilevelcascadingUtil.js
@@ -1,10 +1,15 @@
+var masterSlaveMap = {};
+var slaveOptionsMap = {};
+
 function dynamicMultiLevelSelect(masterId, slaveId) {
     var masterSelector = "select#" + masterId;
     var slaveSelector = "select#" + slaveId;
+    masterSlaveMap[masterSelector] = slaveSelector;
 jQuery(document).ready(function($) {
     // Code that uses jQuery's $ can follow here.
 
     var slaveOptions = $("option", slaveSelector);
+    slaveOptionsMap[slaveSelector] = slaveOptions;
     $(masterSelector).change(
         function(event) {
             $(slaveSelector).hide();
@@ -50,9 +55,39 @@ jQuery(document).ready(function($) {
     var option = $("option[value='"+value+"']", select);
     if (option.size()>0) {
         $(select).attr("selectedIndex", option.get(0).index);
-        $(select).trigger("change");
-    } else {
-        clickFirstVisible(select);
+        while (select in masterSlaveMap) {
+            select = masterSlaveMap[select];
+
+            // selectOption is only called on the page load.  We don't
+            // want to trigger anything on page load, since we can't
+            // guarrantee that things will get executed in the correct
+            // order.  So we crib code from dynamicMultiLevelSelect
+            // but we use a loop instead of depending on the change event.
+
+            var slaveOptions = slaveOptionsMap[select];
+            $(select).hide();
+            $(select).empty();
+            var options = slaveOptions.filter("."+value);
+            options.filter("[value='-1']").each(
+                function() {
+                    $(select).append(this);
+                }
+            );
+            slaveOptions.filter(".select").each(
+                function() {
+                    $(select).append(this);
+                }
+            );
+            options.filter("[value!='-1']").each(
+                function() {
+                    $(select).append(this);
+                }
+            );
+
+            $(select).show();
+            $(select).attr("selectedIndex", 0);
+            value = $(select).val();
+        }
     }
   });
 }
